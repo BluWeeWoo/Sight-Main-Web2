e07f88c07d5c0aabda19229631d8809ab49343e2
Achilles Vonn Rabina <140421417+achllzvr@users.noreply.github.com>
2026-05-03 01:17:58 +0800

    feat: Add Sanctum API token support to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The current schema does not consistently provide an updated_at column.
